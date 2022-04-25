46ead71f7e3fd4a279432e2f9e751c575785a61c
LuckyWind_sck <luckywindsck@gmail.com>
2022-04-25 22:43:53 +0900

    docs: add explanation of map data format

--- src/map.js
+++ src/map.js
@@ -1,3 +1,12 @@
+// Map is a 2d-array of dimention r * c
+// `r` is the row number, `c` is the column number
+//
+// Each element represent the state of a cell:
+// * null - empty cell
+// * 0 - unoccupied cell
+// * 1 - respawn cell of player 1
+// * 2 - respawn cell of player 2
+
 module.exports = [
   [
     null,
